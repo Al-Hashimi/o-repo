f77487d5ff7fe31dbc25b7e5df55e9d171b200d3
Al-Hashimi <alhashimi_ali2002>
2020-06-08 00:14:35 +0200

    update app.py, add docker file

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
 import os
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcom!"
+    return "Welcome!"
 
-@app.route("/how are you")
+@app.route('/how are you')
 def hello():
-    return "I am good, how about you"
+    return 'I am good, how about you?'
+
+@app.route('/how are you1')
+def hello():
+    return 'I am good, how about you1?'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run()
